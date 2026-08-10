02c4373f44376578d3e9d50ac04b0c3b51811e83
Samed Tevin <samedtevin57@gmail.com>
2026-08-10 22:55:39 +0300

    polish: Integrate cards to specify the area of topics and update on ImageViews

--- app/src/main/java/com/samedtevin/bagcilarapp/ui/main/ReportFragment.kt
+++ app/src/main/java/com/samedtevin/bagcilarapp/ui/main/ReportFragment.kt
@@ -5,18 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.samedtevin.bagcilarapp.R
+import com.samedtevin.bagcilarapp.databinding.FragmentReportBinding
 
 
 class ReportFragment : Fragment() {
 
+    private var _binding: FragmentReportBinding? = null
+    val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false)
+        _binding = FragmentReportBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_reportFragment_to_reportReviewFragment)
+        }
     }
 
 
